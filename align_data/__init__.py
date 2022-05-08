3c26922cd117ad13e058ab0b92ea1d01912a7bd6
dan <da.clothiaux@gmail.com>
2022-05-08 17:27:19 -0600

    added code to download reports and nonarxiv papers

--- align_data/__init__.py
+++ align_data/__init__.py
@@ -2,7 +2,8 @@ import align_data.blogs
 import align_data.ebooks
 import align_data.arxiv_papers
 
-DATASET_REGISTRY = blogs.BLOG_REGISTRY + ebooks.EBOOK_REGISTRY + arxiv_papers.ARXIV_REGISTRY
+DATASET_REGISTRY = blogs.BLOG_REGISTRY + ebooks.EBOOK_REGISTRY + arxiv_papers.ARXIV_REGISTRY + \
+                   nonarxiv_papers.NONARXIV_PAPER_REGISTRY + reports.REPORT_REGISTRY
 
 ALL_DATASETS = sorted([dataset.name for dataset in DATASET_REGISTRY])
 DATASET_MAP = dict([(dataset.name, dataset) for dataset in DATASET_REGISTRY])
